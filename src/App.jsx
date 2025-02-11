7e5416ce1b475e1622d1f0791fe2e044e3e63635
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-11 15:01:19 -0500

    navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Navbar from "./components/Navbar";
 
-const App = () =>{
+const App = () => {
   return (
     <div>
-    <Home/>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,9 @@
 
 const Favorites = () =>{
   return (
-    <div>
-
+    <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
   );
 }
